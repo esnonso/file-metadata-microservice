0dd6c69472563e836f125bcfaf1b8f0719c9ef82
esnonso <devnellynonso@gmail.com>
2021-08-26 12:48:04 +0100

    Complete Project

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var cors = require('cors');
 require('dotenv').config()
 
 var app = express();
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' });
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,8 +13,10 @@ app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+app.post('/api/fileanalyse', upload.single('upfile'),(req, res) => {
+  const {originalname, mimetype, size} = req.file;
+  res.json({name:originalname, type:mimetype, size:size})
+})
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
